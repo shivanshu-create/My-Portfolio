7553f82b375782196dd43cb42ac20b7e9d8be6d7
shivanshu techie <shivanshup938@gmail.com>
2026-08-18 01:22:47 +0530

    Skills, Projects(ui) & Contact sections has been completed.

---
+++ main.js
@@ -0,0 +1,79 @@
+const projects = [
+  {
+    name: "Notelet",
+    description: "Linux Text editor with Restore Session, Pin and many more abilities"
+  },
+  {
+    name: "ClipCycle",
+    description: "Middle click paste with copy on selection, stores every copied item"
+  },
+  {
+    name: "Resilient Gateway",
+    description: "Distributed Rate Limiting & Circuit Breaking"
+  }
+];
+
+function createProjectCard(project) {
+  return `
+    <div class="container">
+      <div class="box">
+        <span class="title">${project.name}</span>
+        <div>
+          <p>${project.description}</p>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+const projectContainer = document.getElementById("projects");
+
+projects.forEach(project => {
+  projectContainer.innerHTML += createProjectCard(project);
+});
+
+const skills = [
+  {
+    skill_domain: "Languages",
+    skill_name: ["C","C++","Python","JavaScript","TypeScript","SQL","Lua"]
+  },
+  {
+    skill_domain: "Web Development",
+    skill_name: ["HTML","CSS","React","Tailwind","REST APIs","WebExtensions API","API Design"]
+  },
+  {
+    skill_domain: "Backend & Development",
+    skill_name: ["Node.js","Express.js","MongoDB","PostgreSQL","Redis","Git","GitHub","Docker","Docker Compose","VS Code"]
+  },
+  {
+    skill_domain: "Libraries & Frameworks",
+    skill_name: ["OpenCV","GTK4","Libadwaita"]
+  },
+  {
+    skill_domain: "Systems & Engineering",
+    skill_name: ["Data Structures & Algorithms","Object-Oriented Programming","Concurrency","Distributed Systems","Rate Limiting","Circuit Breaking"]
+  },
+  {
+    skill_domain: "Operating Systems",
+    skill_name: ["Windows","Linux (Ubuntu)"]
+  },
+];
+
+function createSkillsPart(skill) {
+  skillsNames = skill.skill_name
+    .map(name => `<span class="skill-name">${name}</span>`)
+    .join("");
+
+  return `
+    <div class="Skill">
+        <h4>${skill.skill_domain}</h4>
+        ${skillsNames}
+    </div>
+  `
+}
+
+const skillsContainer = document.getElementById("skills-container")
+
+skills.forEach(skill => {
+  skillsContainer.innerHTML += createSkillsPart(skill)
+});
